6acc3ad6aad3f341f821f2e992de966a7cd5d695
BT_1N3_08 <BT_1N3_08@BT_1N3_08.bktech.local>
2018-01-18 09:23:06 -0500

    Added Stuff

--- In-PlaceSorts/src/SearchAlgorithms.java
+++ In-PlaceSorts/src/SearchAlgorithms.java
@@ -1,13 +1,23 @@
 
 public class SearchAlgorithms {
-
+	int[]nums = {45, 40, 7, 20, 16, 7, 25, 18, 20, 47};
 	public static void Swap(int[]nums, int index, int index1)
 	{
 		int temp = nums[index1];
 		nums[index] = nums[index1];
 		nums[index1] = temp;
 	}
-}
+
+	public static void InsertionSort(int[]nums)
+	{
+		
+	}
+	public static void SelectionSort(double[]nums)
+	{
+		
+	}
+
+	}
 
 
 
