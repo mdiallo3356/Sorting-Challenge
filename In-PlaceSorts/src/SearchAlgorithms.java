e43a969e890f55fad728f631e9b7fc6219fd6150
BT_1N3_08 <BT_1N3_08@BT_1N3_08.bktech.local>
2018-01-18 09:02:16 -0500

    #55bound

---
+++ In-PlaceSorts/src/SearchAlgorithms.java
@@ -0,0 +1,13 @@
+
+public class SearchAlgorithms {
+
+	public static void Swap(int[]nums, int index, int index1)
+	{
+		int temp = nums[index1];
+		nums[index] = nums[index1];
+		nums[index1] = temp;
+	}
+}
+
+
+
